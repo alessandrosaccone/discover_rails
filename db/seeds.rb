5fc1813bc4f74c701527804b2dfc3b4dca41f4a9
alessandrosaccone <alessandrosaccone96@gmail.com>
2023-05-08 23:39:38 +0200

    aggiorno

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,8 @@ u1 = User.create({ name: 'Sally', email: 'sally@example.com', password: 'aaaaaaa
 u2 = User.create({ name: 'Sue', email: 'sue@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', user_role: r2.id })
 u3 = User.create({ name: 'Kev', email: 'kev@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', user_role: r2.id })
 u4 = User.create({ name: 'Jack', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', user_role: r1.id })
+
+#p1 = Post.create({ descrizione: 'Evviva la vita regaz', titolo: 'Visita al vaticansbro', data: '11/11/2023', ora: '10:00',
+#    lingua: 'Italiano', persone: '10', prezzo: '60', nomeC: 'Roma', regioneC: 'Lazio', nomeP: 'Roma'})
+#p2 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro2', data: '11/11/2024', ora: '10:00',
+#    lingua: 'Tedesco', persone: '15', prezzo: '80', nomeC: 'Formia', regioneC: 'Lazio', nomeP: 'Latina'})
