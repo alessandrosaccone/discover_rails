a4902c58d5511050d3f7a4a587938eeba9d046f9
palu001 <palumbosimone80@gmail.com>
2023-05-10 21:48:30 +0200

    Asincrono

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,15 +10,24 @@ class PagesController < ApplicationController
         @guides = User.where(role_id: 10)
         nome = params[:nome]
         citta = params[:citta]
-      
+        linguaggio=params[:linguaggio]
         # Modifica la variabile @guides in base agli input del form (nome e citta)
         if nome.present?
-          @guides = @guides.where("LOWER(name) LIKE ?", "%#{nome.downcase}%")
+          @guides = @guides.where("LOWER(users.name) LIKE ?", "%#{nome.downcase}%")
         end
         if citta.present?
           @guides = @guides.joins(:city).where("LOWER(cities.name) LIKE ?", "%#{citta.downcase}%")
         end
-
+        if linguaggio.present?
+          @guides = @guides.joins(:languages).where("languages.name LIKE ?", "%#{linguaggio}%")
+        end
+        if request.xhr?
+          render partial: 'guides_results', locals: { guides: @guides }
+        else
+          respond_to do |format|
+            format.html { render 'guides' }
+          end
+        end
     end
       
 end
