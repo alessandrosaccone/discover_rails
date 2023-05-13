6ec7c2efc89f76b1a6d26c894064c23bb00da1f3
Gabriele Matini <gabriele.matini18@gmail.com>
2023-05-13 22:14:35 +0200

    m

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_13_162026) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_13_173941) do
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
@@ -119,9 +119,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_13_162026) do
     t.string "provider"
     t.string "uid"
     t.integer "city_id"
-    t.string "stripe_account_id"
     t.string "lat"
     t.string "long"
+    t.string "stripe_account_id"
     t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
