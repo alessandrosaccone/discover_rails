e249bd8f5b3fcdbe9a76da3f47fd17daf6b44c26
Gabriele Matini <gabriele.matini18@gmail.com>
2023-05-15 21:33:12 +0200

    messagistica tempo reale

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-#r1 = Role.create({ name: 'Tourist', description: 'Can search for guides, normal user' })
-#r2 = Role.create({ name: 'Guide', description: 'Can post tour announcements and do his job on the platform' })
+r1 = Role.create({ name: 'Tourist', description: 'Can search for guides, normal user' })
+r2 = Role.create({ name: 'Guide', description: 'Can post tour announcements and do his job on the platform' })
 #
-#u1 = User.create({ name: 'Sally', email: 'sally@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', user_role: r1.id })
-#u2 = User.create({ name: 'Sue', email: 'sue@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', user_role: r2.id })
-#u3 = User.create({ name: 'Kev', email: 'kev@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', user_role: r2.id })
-#u4 = User.create({ name: 'Jack', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: 10})
-#u4 = User.create({ name: 'Jacke', email: 'jacke@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: 10})
-#u5 = User.create({ name: 'Jackel', email: 'jackel@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: 10, lat: 41.9079616, long: 12.514760})
-#l1=Language.create(name: "Francese")
-#l2=Language.create(name:"Italiano")
-#l3=Language.create(name:"Inglese")
-#c1=City.create(name: "Roma", region: "Lazio")
-#c2=City.create(name: "Frosinone", region: "Lazio")
+u1 = User.create({ name: 'Sally', email: 'sally@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r1.id })
+u2 = User.create({ name: 'Sue', email: 'sue@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r2.id })
+u3 = User.create({ name: 'Kev', email: 'kev@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r2.id })
+u4 = User.create({ name: 'Jack', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r1.id})
+u4 = User.create({ name: 'Jacke', email: 'jacke@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r2.id})
+u5 = User.create({ name: 'Jackel', email: 'jackel@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r1.id, lat: 41.9079616, long: 12.514760})
+l1=Language.create(name: "Francese")
+l2=Language.create(name:"Italiano")
+l3=Language.create(name:"Inglese")
+c1=City.create(name: "Roma", region: "Lazio")
+c2=City.create(name: "Frosinone", region: "Lazio")
 
 p1 = Post.create({ descrizione: 'Evviva la vita regaz', titolo: 'Visita al vaticansbro', data: '11/01/2023', ora: '10:00',
   lingua: 'Italiano', persone: '10', prezzo: '60', nomeC: 'Roma', regioneC: 'Lazio', nomeP: 'Roma', numero_ore: 3, user_email: 'sally@example.com', address: 'Via Nicola Salvi'})
-#p2 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro2', data: '11/11/2024', ora: '10:00',
-#    lingua: 'Tedesco', persone: '15', prezzo: '80', nomeC: 'Formia', regioneC: 'Lazio', nomeP: 'Latina', numero_ore: 4, user_email: 'sue@example.com', address: 'Via Nicola Salvi 3'})
-#p3 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro3', data: '11/06/2024', ora: '15:00',
-#    lingua: 'Inglese', persone: '8', prezzo: '50', nomeC: 'Napoli', regioneC: 'Campania', nomeP: 'Napoli', numero_ore: 2, user_email: 'jacke@example.com', address: 'Via della Minerva 1'})
-#p4 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro4', data: '8/11/2024', ora: '10:00',
-#    lingua: 'Tedesco', persone: '15', prezzo: '80', nomeC: 'Formia', regioneC: 'Lazio', nomeP: 'Latina', numero_ore: 4, user_email: 'jacke@example.com', address: 'Via della Minerva 10'})
-#p5 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro4', data: '8/11/2024', ora: '10:00',
-#    lingua: 'Tedesco', persone: '15', prezzo: '80', nomeC: 'Formia', regioneC: 'Lazio', nomeP: 'Latina', numero_ore: 4, user_email: 'sue@example.com'})
+p2 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro2', data: '11/11/2024', ora: '10:00',
+    lingua: 'Tedesco', persone: '15', prezzo: '80', nomeC: 'Formia', regioneC: 'Lazio', nomeP: 'Latina', numero_ore: 4, user_email: 'sue@example.com', address: 'Via Nicola Salvi 3'})
+p3 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro3', data: '11/06/2024', ora: '15:00',
+    lingua: 'Inglese', persone: '8', prezzo: '50', nomeC: 'Napoli', regioneC: 'Campania', nomeP: 'Napoli', numero_ore: 2, user_email: 'jacke@example.com', address: 'Via della Minerva 1'})
+p4 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro4', data: '8/11/2024', ora: '10:00',
+    lingua: 'Tedesco', persone: '15', prezzo: '80', nomeC: 'Formia', regioneC: 'Lazio', nomeP: 'Latina', numero_ore: 4, user_email: 'jacke@example.com', address: 'Via della Minerva 10'})
+p5 = Post.create({ descrizione: 'Evviva la vita regaz2', titolo: 'Visita al vaticansbro4', data: '8/11/2024', ora: '10:00',
+   lingua: 'Tedesco', persone: '15', prezzo: '80', nomeC: 'Formia', regioneC: 'Lazio', nomeP: 'Latina', numero_ore: 4, user_email: 'sue@example.com'})
 
  
