e52649144a28daa8844f131a0b6d9c24d05c10c2
Gabriele Matini <gabriele.matini18@gmail.com>
2023-06-01 20:41:07 +0200

    setup bg job update post status

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,18 @@ class Post < ApplicationRecord
     has_many :users, through: :bookings
     validates :data, presence: true
     
+
+
+    def expired?
+        DateTime.now > expiration_date
+    end
+    
     private 
     def set_persone_rimanenti_default
         self.persone_rimanenti = self.persone
+    end
+
+    def expiration_date
+        DateTime.new(data.year,data.month,data.day,ora.hour)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_105924) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_181340) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -117,6 +117,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_105924) do
     t.string "user_email"
     t.integer "user_id"
     t.string "address"
+    t.string "status", default: "open", null: false
   end
 
   create_table "roles", force: :cascade do |t|
