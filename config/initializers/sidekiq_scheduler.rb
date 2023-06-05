89238fb926ff2225a4e7de603b2c2abc68266ffd
Gabriele Matini <gabriele.matini18@gmail.com>
2023-06-05 20:38:50 +0200

    d

--- config/initializers/sidekiq_scheduler.rb
+++ config/initializers/sidekiq_scheduler.rb
@@ -6,7 +6,8 @@ Sidekiq.configure_server do |config|
     Sidekiq.schedule = YAML.load_file(File.expand_path('../../../config/schedule.yml', __FILE__))
     SidekiqScheduler::Scheduler.reload_schedule!
     puts "I'm calling schedule_job"
-    #CheckDateJob.schedule_job
+    CheckDateJob.schedule_job
+    UpdatePostStatus.schedule_job
   end
 
  
