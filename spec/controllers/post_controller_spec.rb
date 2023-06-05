55cce6c1ac796516f944848f270666146aaaf496
alessandrosaccone <alessandrosaccone96@gmail.com>
2023-06-05 20:11:17 +0200

    aggiorno

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -55,50 +55,6 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  describe '#get_price' do
-    let(:post) { double('Post', prezzo: 10, numero_ore: 2, persone: 5, persone_rimanenti: 5) }
-    let(:params) { { id: 1, persone: 3 } }
-
-    before do
-      allow(Post).to receive(:find).and_return(post)
-    end
-
-    context 'when the price calculation is successful' do
-      it 'returns the calculated price' do
-        get :get_price, params: params
-
-        expect(response).to have_http_status(:ok)
-        expect(JSON.parse(response.body)).to eq({ 'price' => 12 })
-      end
-    end
-
-    context 'when the number of people is zero or negative' do
-      let(:params) { { id: 1, persone: -1 } }
-
-      it 'returns an error response' do
-        get :get_price, params: params
-
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(JSON.parse(response.body)).to eq({ 'error' => 'Inserisci un numero positivo di persone' })
-      end
-    end
-
-    context 'when the number of people exceeds the available limit' do
-      let(:params) { { id: 1, persone: 3 } }
-
-      before do
-        allow(post).to receive(:persone_rimanenti).and_return(2)
-      end
-
-      it 'returns an error response' do
-        get :get_price, params: params
-
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(JSON.parse(response.body)).to eq({ 'error' => 'Inserisci un numero entro il limite di persone' })
-      end
-    end
-  end
-
   describe '#all' do
     let(:posts) { [double('Post')] }
 
