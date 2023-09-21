24c4bd29d5953283e326fe2c8aae790ee98347c9
alessandrosaccone <alessandrosaccone96@gmail.com>
2023-09-21 23:10:54 +0200

    Now if you choose a language it remains the same in all the pages of the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,30 @@
 class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_locale
 
+    def switch_language
+        chosen_language = params[:language] || I18n.default_locale
+    
+        puts "Language selected: #{chosen_language}"
+        
+        # Store the chosen language in the session
+        session[:language] = chosen_language
+    
+        I18n.locale = chosen_language
+        redirect_back(fallback_location: root_path)
+    end
+=begin    
     def switch_language
         puts "Language selected: #{params[:language]}"
         I18n.locale = params[:language] || I18n.default_locale
         redirect_back(fallback_location: root_path)
+    end
+=end
+    private
+
+    def set_locale
+        I18n.locale = session[:language] || I18n.default_locale
     end
 
     protected
