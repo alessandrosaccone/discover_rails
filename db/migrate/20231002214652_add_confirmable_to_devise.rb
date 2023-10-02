6380f0f48deee58690d07a305b56d1872c96ae06
alessandrosaccone <alessandrosaccone96@gmail.com>
2023-10-03 00:33:00 +0200

    Added the confirmation of the email with devise

---
+++ db/migrate/20231002214652_add_confirmable_to_devise.rb
@@ -0,0 +1,16 @@
+class AddConfirmableToDevise < ActiveRecord::Migration[7.0]
+  def up
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
+    add_index :users, :confirmation_token, unique: true
+    # User.reset_column_information # Uncomment this line if you're using older versions of Rails
+    # User.all.update_all confirmed_at: DateTime.now # Uncomment this line to default all existing users to confirmed
+  end
+
+  def down
+    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
+    remove_columns :users, :unconfirmed_email # Only if using reconfirmable
+  end
+end
