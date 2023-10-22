1c838246778ffce90fded15e6b447d4235a7ab90
alessandrosaccone <alessandrosaccone96@gmail.com>
2023-10-22 12:20:39 +0200

    Solved the bug for viewing the posts in Visit section

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_10_03_121026) do
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_214652) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,20 @@
 r1 = Role.create({ id: 9, name: 'Tourist', description: 'Can search for guides, normal user' })
 r2 = Role.create({ id: 10, name: 'Guide', description: 'Can post tour announcements and do his job on the platform' })
 
+u1 = User.create({name: "sae", email: "sae@example.it", password: "aaaaaaaa"})
 
+p1 = Post.create({descrizione: "Ciao", titolo: "Ciao ciao", data: "12/12/2024", lingua: "italiano", persone: "15",
+  prezzo: "50", nomeC: "Roma", regioneC: "Lazio", nomeP: "Roma", numero_ore: "3", user_email: "sae@example.it", address: "
+  Via della Brianza 2"})
+p2 = Post.create({descrizione: "Ciao", titolo: "Ciao ciao", data: "12/12/2024", lingua: "italiano", persone: "15",
+    prezzo: "50", nomeC: "Roma", regioneC: "Lazio", nomeP: "Roma", numero_ore: "3", user_email: "sae@example.it", address: "
+    Via della Brianza 2"})
+p3 = Post.create({descrizione: "Ciao", titolo: "Ciao ciao", data: "12/12/2024", lingua: "italiano", persone: "15",
+      prezzo: "50", nomeC: "Roma", regioneC: "Lazio", nomeP: "Roma", numero_ore: "3", user_email: "sae@example.it", address: "
+      Via della Brianza 2"})
+p4 = Post.create({descrizione: "Ciao", titolo: "Ciao ciao", data: "12/12/2024", lingua: "italiano", persone: "15",
+        prezzo: "50", nomeC: "Roma", regioneC: "Lazio", nomeP: "Roma", numero_ore: "3", user_email: "sae@example.it", address: "
+        Via della Brianza 2"})
+p5 = Post.create({descrizione: "Ciao", titolo: "Ciao ciao", data: "12/12/2024", lingua: "italiano", persone: "15",
+          prezzo: "50", nomeC: "Roma", regioneC: "Lazio", nomeP: "Roma", numero_ore: "3", user_email: "sae@example.it", address: "
+          Via della Brianza 2"})
