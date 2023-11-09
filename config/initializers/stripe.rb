fb84c1e1e76eb31aacfd3498a8da1aeca2ddd57a
alessandrosaccone <alessandrosaccone96@gmail.com>
2023-11-09 22:34:32 +0100

    Securing stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
     publishable_key: 'pk_test_51N6vy0A60U57XjZBghMppU24yDeSC7wQb1HSqcOcZU12t1MKgYLWxhAfF5xxOD58jvM4WXdvMmnP5sFH0mVRB98700XRX8RXEl',
-    secret_key: 'sk_test_51N6vy0A60U57XjZBEUBJe19s82Xuqr2wBSMNgfdec5pJBQIOHEZT1tLhOBs1aCRz1wKnA4tWy1Z7L694xoLWRjZD00gdUMtNi7'
+    secret_key: ENV['STRIPE_SECRETKET']
   }
   
   Stripe.api_key = Rails.configuration.stripe[:secret_key]
